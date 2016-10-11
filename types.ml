3a0837e619ffa104f4d2ef8fb7a9647ecb38ecd1
Joe Groocock <frebib@gmail.com>
2016-10-11 17:57:04 +0100

    Add print functions for debugging

--- types.ml
+++ types.ml
@@ -1,3 +1,5 @@
+open List
+
 type opcode =
   | Plus | Minus | Times | Divide
   | Leq | Geq | Equal | Noteq
@@ -23,8 +25,43 @@ type expression =
 type fundef = Func of string * string list * expression 
 type program = fundef list
 
+let esc s = "\"" ^ s ^ "\""
 let rec flatten_exp = function
   | []      -> Empty
+  | [x]     -> x
   | x :: xs -> Seq (x, flatten_exp xs)
-;;
 
+and unflatten_exp = function
+  | Seq (hd, tl) -> hd :: unflatten_exp tl
+  | Empty        -> []
+  | e            -> [e]
+  (* failwith ("Can't unflatten a non-Seq expression: " ^ string_of_exp e) *)
+
+(* Why this isn't implicit I have no idea *)
+and string_of_op = function
+    | Plus -> "Plus" | Minus -> "Minus" | Times -> "Times"
+    | Divide -> "Divide" | Leq -> "Leq" | Geq -> "Geq" | Equal -> "Equal"
+    | Noteq -> "NotEq" | And -> "And" | Or -> "Or" | Not -> "Not"
+
+and string_of_exp = function
+    | Empty                 -> "Empty"
+    | Seq (hd, tl)          -> "Seq (" ^ (unflatten_exp (Seq (hd, tl)) |> map string_of_exp |> String.concat ", ") ^ ")" 
+    | While (e, f)          -> "While (" ^ string_of_exp e ^ ") { " ^ string_of_exp f ^ " }"
+    | For (a, b, c, d)      -> "For (" ^ (map string_of_exp [a;b;c] |> String.concat "; ") ^ ") {" ^ string_of_exp d ^ "}"
+    | If (e, a, b)          -> "If (" ^ string_of_exp e ^ ") { " ^ (match b with
+                                | Empty -> " }"
+                                | _     -> " } Else { " ^ string_of_exp b ^ " }")
+    | Asg (x, v)            -> string_of_exp x ^ " = " ^ string_of_exp v
+    | Deref x               -> string_of_exp x
+    | Operator (op, e1, e2) -> string_of_exp e1 ^ string_of_op op ^ string_of_exp e2
+    | Application (i, e)    -> string_of_exp i ^ "(" ^ string_of_exp e ^ ")"
+    | Const n               -> "Const " ^ string_of_int n
+    | Readint               -> "read_int()"
+    | Printint e            -> "print_int(" ^ string_of_exp e ^ ")"
+    | Identifier s          -> "Identifier " ^ esc s
+    | Let (x, v, e)         -> "Let " ^ x ^ " = " ^ string_of_exp v ^ " in " ^ string_of_exp e
+    | New (x, v, e)         -> "New " ^ x ^ " = " ^ string_of_exp v ^ " in " ^ string_of_exp e
+
+and string_of_func = function
+    | Func (name, args, exp) -> "Function " ^ esc name ^ "(" ^ String.concat ", " args ^
+                                    ") { " ^ string_of_exp exp ^ " }"
