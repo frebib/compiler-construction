7eb05917307e389a3abc4e2a8ae14f7d04c66566
Joe Groocock <frebib@gmail.com>
2016-10-11 17:58:21 +0100

    Catch and print useful parse errors

--- main.ml
+++ main.ml
@@ -1,10 +1,23 @@
+open Types
+
 let usage () = print_string ("Usage: " ^ Sys.argv.(0) ^ " <filename>")
     |> print_newline;;
 
+let safe_parse lexbuf = try
+    Parser.init Lexer.read lexbuf with
+    | Parser.Error          -> print_string ("Error: " ^ (Lexing.lexeme lexbuf))
+                               |> print_newline;
+                               exit (-1)
+    | Lexer.SyntaxError msg -> print_string msg
+                               |> print_newline;
+                               exit (-1)
+
 let parse file = open_in file
     |> Lexing.from_channel
-    |> Parser.init Lexer.read
-    |> ignore
+    |> safe_parse
+    |> List.map Types.string_of_func
+    |> String.concat "\n"
+    |> print_string
 
 let _ =
     if Array.length Sys.argv < 2
