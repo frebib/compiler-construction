a73de21c8cc6aaeca0810a2adb6e98f133122ed0
Joe Groocock <frebib@gmail.com>
2016-10-12 17:19:23 +0100

    Print more useful error messages

--- src/main.ml
+++ src/main.ml
@@ -1,23 +1,29 @@
 open Types
+open Printf
 
 let usage () = print_string ("Usage: " ^ Sys.argv.(0) ^ " <filename>")
     |> print_newline;;
 
-let safe_parse lexbuf = try
-    Parser.init Lexer.read lexbuf with
-    | Parser.Error          -> print_string ("Error: " ^ (Lexing.lexeme lexbuf))
-                               |> print_newline;
+let location_message filename lexbuf = 
+    let offs = Lexing.lexeme_start_p lexbuf in
+    sprintf "%s: at line %d, column %d" filename offs.pos_lnum (offs.pos_cnum - offs.pos_bol + 1)
+
+let safe_parse filename lexbuf = try Parser.init Lexer.read lexbuf with
+    | Parser.Error          -> eprintf "Parse error: %s\n%s\n" 
+                                 (Lexing.lexeme lexbuf)
+                                 (location_message filename lexbuf);
                                exit (-1)
-    | Lexer.SyntaxError msg -> print_string msg
-                               |> print_newline;
-                               exit (-1)
+    | Lexer.SyntaxError msg -> eprintf "%s\n%s\n" msg
+                                 (location_message filename lexbuf);
+                                 exit (-1)
 
 let parse file = open_in file
     |> Lexing.from_channel
-    |> safe_parse
+    |> safe_parse file
     |> List.map Types.string_of_func
     |> String.concat "\n"
     |> print_string
+    |> print_newline
 
 let _ =
     if Array.length Sys.argv < 2
