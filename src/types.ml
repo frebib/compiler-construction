c6f4f950d2f24ade77efdc81714632e0dd4af08d
Joe Groocock <frebib@gmail.com>
2016-10-14 00:25:06 +0100

    Fix incorrect indentation

--- src/types.ml
+++ src/types.ml
@@ -48,28 +48,28 @@ and unflatten_exp = function
 
 (* string_of_x functions mostly for debugging *)
 and string_of_op = function
-    | Plus -> "+" | Minus -> "-" | Times -> "*" |  Divide -> "/"
-    | Lth -> "<" | Gth -> ">" | Leq -> "<=" | Geq -> ">="
-    | Equal -> "==" | Noteq -> "!=" | And -> "&&" | Or -> "||" | Not -> "!"
+  | Plus -> "+" | Minus -> "-" | Times -> "*" |  Divide -> "/"
+  | Lth -> "<" | Gth -> ">" | Leq -> "<=" | Geq -> ">="
+  | Equal -> "==" | Noteq -> "!=" | And -> "&&" | Or -> "||" | Not -> "!"
 
 and string_of_exp = function
-    | Empty                 -> "Empty"
-    | Seq (hd, tl)          -> "Seq [" ^ (unflatten_exp (Seq (hd, tl)) |> map string_of_exp |>  String.concat ", ") ^ "]"
-    | While (e, f)          -> "While (" ^ string_of_exp e ^ ") { " ^ string_of_exp f ^ " }"
-    | If (e, a, b)          -> "If " ^ ([e;a;b] |> map string_of_exp |> map wrap |> String.concat ", ")
-    | Asg (x, v)            -> "Assign " ^ string_of_exp x ^ " = " ^ string_of_exp v
-    | Deref x               -> "Deref " ^ wrap (string_of_exp x)
-    | Operator (op, e1, e2) -> string_of_exp e1 ^ " " ^ string_of_op op ^ " " ^ string_of_exp e2
-    | Application (i, e)    -> "Application " ^ wrap (string_of_exp i) ^ ", " ^ wrap (string_of_exp e)
-    | Const n               -> "Const " ^ string_of_int n
-    | Boolean b             -> string_of_bool b
-    | Readint               -> "read_int()"
-    | Printint e            -> "print_int(" ^ string_of_exp e ^ ")"
-    | Identifier s          -> "Identifier " ^ esc s
-    | Let (x, v, e)         -> "Let " ^ esc x ^ " = " ^ wrap (string_of_exp v) ^ " in " ^ wrap (string_of_exp e)
-    | New (x, v, e)         -> "New " ^ esc x ^ " = " ^ string_of_exp v ^ " in " ^ string_of_exp e
-    | Return e              -> "Return " ^ wrap (string_of_exp e)
+  | Empty                 -> "Empty"
+  | Seq (hd, tl)          -> "Seq [" ^ (unflatten_exp (Seq (hd, tl)) |> map string_of_exp |>  String.concat ", ") ^ "]"
+  | While (e, f)          -> "While (" ^ string_of_exp e ^ ") { " ^ string_of_exp f ^ " }"
+  | If (e, a, b)          -> "If " ^ ([e;a;b] |> map string_of_exp |> map wrap |> String.concat ", ")
+  | Asg (x, v)            -> "Assign " ^ string_of_exp x ^ " = " ^ string_of_exp v
+  | Deref x               -> "Deref " ^ wrap (string_of_exp x)
+  | Operator (op, e1, e2) -> string_of_exp e1 ^ " " ^ string_of_op op ^ " " ^ string_of_exp e2
+  | Application (i, e)    -> "Application " ^ wrap (string_of_exp i) ^ ", " ^ wrap (string_of_exp e)
+  | Const n               -> "Const " ^ string_of_int n
+  | Boolean b             -> string_of_bool b
+  | Readint               -> "read_int()"
+  | Printint e            -> "print_int(" ^ string_of_exp e ^ ")"
+  | Identifier s          -> "Identifier " ^ esc s
+  | Let (x, v, e)         -> "Let " ^ esc x ^ " = " ^ wrap (string_of_exp v) ^ " in " ^ wrap (string_of_exp e)
+  | New (x, v, e)         -> "New " ^ esc x ^ " = " ^ string_of_exp v ^ " in " ^ string_of_exp e
+  | Return e              -> "Return " ^ wrap (string_of_exp e)
 
 and string_of_func = function
-    | (name, args, exp) -> "Function " ^ esc name ^ " [" ^ String.concat ", " (map esc args) ^
+  | (name, args, exp) -> "Function " ^ esc name ^ " [" ^ String.concat ", " (map esc args) ^
                                     "] { " ^ string_of_exp exp ^ " }"
