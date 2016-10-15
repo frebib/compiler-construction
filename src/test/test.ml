966b6a142d1dc9049fce3146c9f4852cf5e6277e
Joe Groocock <frebib@gmail.com>
2016-10-15 02:55:22 +0100

    Align 'expected' output' code with generated code margin
    Signed-off-by: Joe Groocock <frebib@gmail.com>

--- src/test/test.ml
+++ src/test/test.ml
@@ -3,7 +3,12 @@ open Printf
 (* Code, Output *)
 type test = string * string;;
 
+let indent count =
+  let spaces = Bytes.to_string (Bytes.make count ' ') in
+  Str.global_replace (Str.regexp "\n") ("\n" ^ spaces)
+;;
+
 let generate_test = function
-| (code, expected) -> sprintf "open Types\n\nlet _ =\n  let code = \"%s\" in\n  let expected = %s in\n  let actual = Parse.parse \"%%inline%%\" (Lexing.from_string code) in\n  print_string (if actual = expected then \"PASS!\" else \"FAIL\"); print_newline (); Parse.parse_print \"\" (Lexing.from_string code);  print_newline "(String.escaped code) (String.trim expected)
+| (code, expected) -> sprintf "open Types\n\nlet _ =\n  let code = \"%s\" in\n  let expected = %s in\n  let actual = Parse.parse \"%%inline%%\" (Lexing.from_string code) in\n  print_string (if actual = expected then \"PASS!\" else \"FAIL\"); print_newline (); Parse.parse_print \"\" (Lexing.from_string code);  print_newline " (String.escaped code) (indent 2 (String.trim expected))
 ;;
 
