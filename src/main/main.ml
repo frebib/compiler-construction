0093a2b6975eb484120f9f0c8ab1c292ecf5070a
Joe Groocock <frebib@gmail.com>
2016-10-15 03:24:10 +0100

    Fix 'main' printing it's output
    Signed-off-by: Joe Groocock <frebib@gmail.com>

--- src/main/main.ml
+++ src/main/main.ml
@@ -5,5 +5,5 @@ let usage () = printf "Usage: %s <filename>\n" Sys.argv.(0)
 let _ =
   if Array.length Sys.argv < 2
   then (usage (); exit 1)
-  else Parse.parse_from Sys.argv.(1)
+  else Parse.parse_print_from Sys.argv.(1)
 
--- src/main/parse.ml
+++ src/main/parse.ml
@@ -7,9 +7,8 @@ let parse = Error.safe Parser.init Lexer.read
 let parse_print file buf = 
   parse file buf
   |> List.map string_of_func
-  |> List.map wrap
-  |> String.concat ",\n"
-  |> sprintf "[%s]"
+  |> String.concat ";\n"
+  |> sprintf "[\n%s\n]"
   |> print_string
   |> print_newline
 
@@ -18,4 +17,8 @@ let parse_from file =
   |> Lexing.from_channel
   |> parse file
 
+let parse_print_from file =
+  open_in file
+  |> Lexing.from_channel
+  |> parse_print file
 
