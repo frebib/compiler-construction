09fb5b7633be1fd60d640ef53d4791ec84e0a569
Joe Groocock <frebib@gmail.com>
2016-10-17 14:17:51 +0100

    Import Parse library

--- src/main/main.ml
+++ src/main/main.ml
@@ -1,3 +1,4 @@
+open Parse
 open Printf
 
 let usage () = printf "Usage: %s <filename>\n" Sys.argv.(0)
@@ -5,5 +6,5 @@ let usage () = printf "Usage: %s <filename>\n" Sys.argv.(0)
 let _ =
   if Array.length Sys.argv < 2
   then (usage (); exit 1)
-  else Parse.parse_print_from Sys.argv.(1)
+  else parse_print_from Sys.argv.(1)
 
