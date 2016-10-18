88e47503d1a7fbad15f158c11bdc500a88edf2a8
Joe Groocock <frebib@gmail.com>
2016-10-18 23:03:39 +0100

    Inline the usage() function in src/main.ml

--- src/main.ml
+++ src/main.ml
@@ -1,10 +1,8 @@
 open Parse
 open Printf
 
-let usage () = printf "Parses languages and prints the tree\nUsage: %s <filename>\n" Sys.argv.(0)
-
 let _ =
   if Array.length Sys.argv < 2
-  then (usage (); exit 1)
+  then (printf "Parses languages and prints the tree\nUsage: %s <filename>\n" Sys.argv.(0); exit 1)
   else parse_print_from Sys.argv.(1)
 
