0c22e3531d41b3e7a337644e5b56aa66000fc483
Joe Groocock <frebib@gmail.com>
2016-10-19 12:46:35 +0100

    Revert "Inline the usage() function in src/main.ml"
    This reverts commit 88e47503d1a7fbad15f158c11bdc500a88edf2a8.

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,10 @@
 open Parse
 open Printf
 
+let usage () = printf "Parses languages and prints the tree\nUsage: %s <filename>\n" Sys.argv.(0)
+
 let _ =
   if Array.length Sys.argv < 2
-  then (printf "Parses languages and prints the tree\nUsage: %s <filename>\n" Sys.argv.(0); exit 1)
+  then (usage (); exit 1)
   else parse_print_from Sys.argv.(1)
 
