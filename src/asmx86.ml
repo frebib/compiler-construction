41c56349451c19f13047d71b6772ba731d13cfaa
Joe Groocock <frebib@gmail.com>
2016-11-25 00:56:34 +0000

    Zero-extend comparison values and add more comparison operators

--- src/asmx86.ml
+++ src/asmx86.ml
@@ -63,9 +63,23 @@ let rec compile symtbl = function
                             | Divide -> "xor	%rdx, %rdx" |> add_instr;
                                         "idivq	%rbx"     |> add_instr
                             | Equal  -> "cmp	%rbx, %rax" |> add_instr;
-                                        "setz	%al"        |> add_instr
+                                        "setz	%al"        |> add_instr;
+                                        "movsbq %al, %rax"|> add_instr
                             | Noteq  -> "cmp	%rbx, %rax" |> add_instr;
-                                        "setnz	%al"      |> add_instr
+                                        "setnz	%al"      |> add_instr;
+                                        "movsbq %al, %rax"|> add_instr
+                            | Lth    -> "cmp	%rbx, %rax" |> add_instr;
+                                        "setl	%al"        |> add_instr;
+                                        "movsbq %al, %rax"|> add_instr
+                            | Gth    -> "cmp	%rbx, %rax" |> add_instr;
+                                        "setg	%al"        |> add_instr;
+                                        "movsbq %al, %rax"|> add_instr
+                            | Leq    -> "cmp	%rbx, %rax" |> add_instr;
+                                        "setle	%al"      |> add_instr;
+                                        "movsbq %al, %rax"|> add_instr
+                            | Geq    -> "cmp	%rbx, %rax" |> add_instr;
+                                        "setge	%al"      |> add_instr;
+                                        "movsbq %al, %rax"|> add_instr
                             | o -> (match o with
                             | Plus  -> "addq	%rbx, %rax"
                             | Minus -> "subq	%rbx, %rax"
