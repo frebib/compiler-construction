6cec4388aa415bfa432fe2c2f5392c92dfd8635c
Joe Groocock <frebib@gmail.com>
2016-11-25 00:55:12 +0000

    Optimise and remove .cfi anontations fromtemplate code

--- src/asmx86.ml
+++ src/asmx86.ml
@@ -107,37 +107,20 @@ let templ_prefix =
 	.globl	printInt
 	.type	printInt, @function
 printInt:
-.LFB2:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	%edi, -4(%rbp)
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
+	subq	$8, %rsp
+	movl	%edi, %esi
+	xorl	%eax, %eax
 	movl	$.LC0, %edi
-	movl	$0, %eax
 	call	printf
-	nop
-	leave
-	.cfi_def_cfa 7, 8
+	xorl	%eax, %eax
+	addq	$8, %rsp
 	ret
-	.cfi_endproc
-.LFE2:
 	.size	printInt, .-printInt
 	.globl	main
 	.type	main, @function
 main:
-.LFB3:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	subq	$16, %rsp
 	// End template code\n"
 let templ_suffix = 
@@ -147,10 +130,7 @@ let templ_suffix =
 	call	printInt
 	movl	$0, %eax
 	leave
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE3:
 	.size	main, .-main
 	.ident	\"GCC: (GNU) 6.2.1 20160830\"
 	.section	.note.GNU-stack,\"\",@progbits\n"
